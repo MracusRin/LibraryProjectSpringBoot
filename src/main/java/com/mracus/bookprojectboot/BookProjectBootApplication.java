e2c02af47d9ade030fe28c35750f6c6ac0716613
MracusRin <aleksandrovi44@gmail.com>
2022-11-22 23:03:12 +0500

    Init commit

---
+++ src/main/java/com/mracus/bookprojectboot/BookProjectBootApplication.java
@@ -0,0 +1,13 @@
+package com.mracus.bookprojectboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookProjectBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookProjectBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mracus/bookprojectboot/BookProjectBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mracus.bookprojectboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookProjectBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
